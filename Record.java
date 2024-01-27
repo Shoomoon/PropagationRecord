05449a57128591bb82d73eee22e5b459df7ac91a
Shoomoon <informationz@163.com>
2024-01-27 10:51:24 -0800

    Updated record up to 10/26/2023.

---
+++ Record.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class Record {
+    private int id;
+    private String position;
+    private String medium;
+    private int status;
+    private String category;
+    private String species;
+    private Date startTime;
+    private String processingMethod;
+    private String rootingPowerSolutionProcessing;
+    private boolean withRootingPower;
+    private boolean cutMethod;
+    private boolean removeCrushWound;
+    private boolean keepMoisture;
+    private boolean withHeel;
+    private boolean removeBottomBud;
+    private int removeLeave;
+    private String otherProcessingMethod;
+    private String source;
+    private String result;
+    private Date endTime;
+    private String pottingResult;
+    private String resultAnalysis;
+}
